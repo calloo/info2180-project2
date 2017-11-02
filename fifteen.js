2375a82dcb4925567c71c68954fa2aa6ac7beb45
calloo <michael.ma.calloo@gmail.com>
2017-11-02 02:53:47 -0500

    Optimized

--- fifteen.js
+++ fifteen.js
@@ -8,9 +8,6 @@ $(document).ready(function () {
     let freeze = false;
     let image_choice = images[Math.floor(Math.random() * 5)];
 
-    let puzzlePiece = $(".puzzlepiece");
-    let shuffleButton = $("#shufflebutton");
-
     function setup(path) {
         $(".victory").remove();
 
@@ -21,6 +18,7 @@ $(document).ready(function () {
         emptyslot[0] = 300;
         emptyslot[1] = 300;
 
+        //iteratively change the background image and position on each tile
         for (let piece of puzzleSection) {
             piece.style.backgroundImage = 'url("images/' + path + '.jpg")';
 
@@ -39,7 +37,7 @@ $(document).ready(function () {
             scale_x -= 100;
             scale_left += 100;
         }
-        shuffleButton.trigger("click");
+        $("#shufflebutton").trigger("click");
     }
 
 
@@ -74,10 +72,10 @@ $(document).ready(function () {
 
 
     $(".bgImage").click(function () {
-        setup(this.innerHTML);
+        setup(this.innerHTML); // changes the puzzle image
     });
 
-    puzzlePiece.mouseenter(function () {
+    $(".puzzlepiece").mouseenter(function () {
         if (freeze) {
             return;
         }
@@ -86,6 +84,7 @@ $(document).ready(function () {
 
         let top_free = true, bottom_free = true, left_free = true, right_free = true;
 
+        /*Checks to see if the tile is located near the boundaries of the puzzle*/
         if (location_left === 300) {
             right_free = false;
         }
@@ -99,6 +98,8 @@ $(document).ready(function () {
             bottom_free = false;
         }
 
+        //iteratively search for tiles that are close by that are in the assumed free slots, and mark the slots
+        //as no longer being free to move the tile
         for (let piece of puzzleSection) {
             let check = piece.style;
             let top_area = parseInt(check.top.slice(0, -2));
@@ -118,20 +119,24 @@ $(document).ready(function () {
             }
         }
 
+        //checks if the tile is movable
         if (top_free || bottom_free || left_free || right_free) {
             this.classList.add("movablepiece");
         }
 
     });
 
-    puzzlePiece.mouseleave(function () {
+    $(".puzzlepiece").mouseleave(function () {
         this.classList.remove("movablepiece");
     });
 
-    puzzlePiece.click(function () {
+    $(".puzzlepiece").click(function () {
+
+        //checks if the game has been frozen and dont move a tile if its true
         if (freeze) {
             return;
         }
+
         let location_left = parseInt(this.style.left.slice(0, -2));
         let location_top = parseInt(this.style.top.slice(0, -2));
 
@@ -140,6 +145,7 @@ $(document).ready(function () {
         scale_top = 0;
         scale_left = 0;
 
+        /*Checks to see if the tile is located near the puzzle boundary*/
         if (location_left === 300) {
             right_free = false;
         }
@@ -153,6 +159,8 @@ $(document).ready(function () {
             bottom_free = false;
         }
 
+        //iteratively searches for tiles that matches the assumed free areas to move, and mark the areas
+        //as no longer being free
         for (let piece of puzzleSection) {
             let check = piece.style;
             let top_area = parseInt(check.top.slice(0, -2));
@@ -174,6 +182,8 @@ $(document).ready(function () {
 
         }
 
+        /*Checks for the free slot to move the tile and make note of where the new free slot will be,
+        * as well as move the tile*/
         if (top_free) {
             this.style.top = (location_top - 100) + 'px';
             emptyslot[0] = location_left;
@@ -193,6 +203,7 @@ $(document).ready(function () {
             emptyslot[1] = location_top;
         }
 
+        //searches to check if the tiles are in order to determine if the user won
         for (let piece of puzzleSection) {
             if (parseInt(piece.style.top.slice(0, -2)) !== scale_top || parseInt(piece.style.left.slice(0, -2)) !== scale_left) {
                 won = false;
@@ -204,42 +215,66 @@ $(document).ready(function () {
             scale_left += 100;
         }
 
+        //checks if the user won, and it wasn't a random win due to shuffing
         if (won && !shuffing) {
             $("#overall").before("<h1 class='victory' style='color: #006600; text-align: center;'>You Won!!</h1>");
-            freeze = true;
+            freeze = true; //freezes tiles that they cant move
         }
 
 
     });
 
-    shuffleButton.click(function () {
+    $("#shufflebutton").click(function () {
+
+        //removes the victory heading if it exists
         $(".victory").remove();
+
+        //indicates that shuffing is occurring
         shuffing = true;
+
+        //allows tiles to move
         freeze = false;
 
+        //shuffle tiles inplace on the board 100 times
         for (let i = 0; i < 100; i++) {
+
+            //checks to see if the empty slot is in the center of the puzzle
             if (emptyslot[0] > 0 && emptyslot[0] < 300 && emptyslot[1] > 0 && emptyslot[1] < 300) {
-                let location = Math.floor(Math.random() * 4) + 1;
+                let location = Math.floor(Math.random() * 4) + 1; // find the tile to move to empty slot
 
+                //moves the tile above to the empty slot
                 if (location === 1) {
+
+                    //search for the tile and trigger its click event to move the tile
                     for (let piece of puzzleSection) {
                         let piece_info = piece.style;
 
-                        if (parseInt(piece_info.top.slice(0, -2)) === emptyslot[1] - 100 && parseInt(piece_info.left.slice(0, -2)) === emptyslot[0]) {
+                        if (parseInt(piece_info.top.slice(0, -2)) === emptyslot[1] - 100 &&
+                            parseInt(piece_info.left.slice(0, -2)) === emptyslot[0]) {
 
                             $("#" + piece.id).trigger("click");
                         }
                     }
-                } else if (location === 2) {
+                }
+                //moves the tile to the right to the empty slot
+                else if (location === 2) {
+
+                    //search for the tile and trigger its click event to move the tile
                     for (let piece of puzzleSection) {
                         let piece_info = piece.style;
 
-                        if (parseInt(piece_info.top.slice(0, -2)) === emptyslot[1] && parseInt(piece_info.left.slice(0, -2)) === emptyslot[0] + 100) {
+
+                        if (parseInt(piece_info.top.slice(0, -2)) === emptyslot[1] &&
+                            parseInt(piece_info.left.slice(0, -2)) === emptyslot[0] + 100) {
 
                             $("#" + piece.id).trigger("click");
                         }
                     }
-                } else if (location === 3) {
+                }
+                //moves the tile to the bottom to the empty slot
+                else if (location === 3) {
+
+                    //search for the tile and trigger its click event to move the tile
                     for (let piece of puzzleSection) {
                         let piece_info = piece.style;
 
@@ -248,7 +283,11 @@ $(document).ready(function () {
                             $("#" + piece.id).trigger("click");
                         }
                     }
-                } else if (location === 4) {
+                }
+                //moves the tile to the left to the empty slot
+                else if (location === 4) {
+
+                    //search for the tile and trigger its click event to move the tile
                     for (let piece of puzzleSection) {
                         let piece_info = piece.style;
 
@@ -259,9 +298,12 @@ $(document).ready(function () {
                     }
                 }
 
-            } else if (emptyslot[0] === 0) {
-                let location = Math.floor(Math.random() * 3) + 1;
+            }
+            //checks to see if the empty slot is along the leftmost column of the puzzle
+            else if (emptyslot[0] === 0) {
+                let location = Math.floor(Math.random() * 3) + 1; // find the tile to move to empty slot
 
+                //moves the tile to the top to the empty slot
                 if (location === 1) {
                     for (let piece of puzzleSection) {
                         let piece_info = piece.style;
@@ -271,7 +313,9 @@ $(document).ready(function () {
                             $("#" + piece.id).trigger("click");
                         }
                     }
-                } else if (location === 2) {
+                }
+                //moves the tile to the right to the empty slot
+                else if (location === 2) {
                     for (let piece of puzzleSection) {
                         let piece_info = piece.style;
 
@@ -280,7 +324,9 @@ $(document).ready(function () {
                             $("#" + piece.id).trigger("click");
                         }
                     }
-                } else if (location === 3) {
+                }
+                //moves the tile to the bottom to the empty slot
+                else if (location === 3) {
                     for (let piece of puzzleSection) {
                         let piece_info = piece.style;
 
@@ -290,9 +336,12 @@ $(document).ready(function () {
                         }
                     }
                 }
-            } else if (emptyslot[0] === 300) {
-                let location = Math.floor(Math.random() * 3) + 1;
+            }
+            //checks to see if the empty slot is along the rightmost column of the puzzle
+            else if (emptyslot[0] === 300) {
+                let location = Math.floor(Math.random() * 3) + 1;// find the tile to move to empty slot
 
+                //moves the tile to the top to the empty slot
                 if (location === 1) {
                     for (let piece of puzzleSection) {
                         let piece_info = piece.style;
@@ -302,7 +351,9 @@ $(document).ready(function () {
                             $("#" + piece.id).trigger("click");
                         }
                     }
-                } else if (location === 2) {
+                }
+                //moves the tile to the left to the empty slot
+                else if (location === 2) {
                     for (let piece of puzzleSection) {
                         let piece_info = piece.style;
 
@@ -311,7 +362,9 @@ $(document).ready(function () {
                             $("#" + piece.id).trigger("click");
                         }
                     }
-                } else if (location === 3) {
+                }
+                //moves the tile to the bottom to the empty slot
+                else if (location === 3) {
                     for (let piece of puzzleSection) {
                         let piece_info = piece.style;
 
@@ -321,9 +374,12 @@ $(document).ready(function () {
                         }
                     }
                 }
-            } else if (emptyslot[1] === 0) {
+            }
+            //checks to see if the empty slot is along the top row of the puzzle
+            else if (emptyslot[1] === 0) {
                 let location = Math.floor(Math.random() * 3) + 1;
 
+                //moves the tile to the left to the empty slot
                 if (location === 1) {
                     for (let piece of puzzleSection) {
                         let piece_info = piece.style;
@@ -333,7 +389,9 @@ $(document).ready(function () {
                             $("#" + piece.id).trigger("click");
                         }
                     }
-                } else if (location === 2) {
+                }
+                //moves the tile to the right to the empty slot
+                else if (location === 2) {
                     for (let piece of puzzleSection) {
                         let piece_info = piece.style;
 
@@ -342,7 +400,9 @@ $(document).ready(function () {
                             $("#" + piece.id).trigger("click");
                         }
                     }
-                } else if (location === 3) {
+                }
+                //moves the tile to the bottom to the empty slot
+                else if (location === 3) {
                     for (let piece of puzzleSection) {
                         let piece_info = piece.style;
 
@@ -352,9 +412,12 @@ $(document).ready(function () {
                         }
                     }
                 }
-            } else if (emptyslot[1] === 300) {
+            }
+            //checks to see if the empty slot is along the bottom row of the puzzle
+            else if (emptyslot[1] === 300) {
                 let location = Math.floor(Math.random() * 3) + 1;
 
+                //moves the tile to the top to the empty slot
                 if (location === 1) {
                     for (let piece of puzzleSection) {
                         let piece_info = piece.style;
@@ -364,7 +427,9 @@ $(document).ready(function () {
                             $("#" + piece.id).trigger("click");
                         }
                     }
-                } else if (location === 2) {
+                }
+                //moves the tile to the right to the empty slot
+                else if (location === 2) {
                     for (let piece of puzzleSection) {
                         let piece_info = piece.style;
 
@@ -373,7 +438,9 @@ $(document).ready(function () {
                             $("#" + piece.id).trigger("click");
                         }
                     }
-                } else if (location === 3) {
+                }
+                //moves the tile to the left to the empty slot
+                else if (location === 3) {
                     for (let piece of puzzleSection) {
                         let piece_info = piece.style;
 
@@ -388,5 +455,5 @@ $(document).ready(function () {
         shuffing = false;
     });
 
-    shuffleButton.trigger("click");
+    $("#shufflebutton").trigger("click"); //triggers the puzzle to be shuffled on page load
 });
